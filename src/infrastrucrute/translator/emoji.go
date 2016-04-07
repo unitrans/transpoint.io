89f65dfd252ba80082f0285c6ca460930f2e127a
Yury Kozyrev <yury.kozyrev@home24.de>
2016-04-07 10:50:16 +0200

    regrx update

--- src/infrastrucrute/translator/emoji.go
+++ src/infrastrucrute/translator/emoji.go
@@ -24,7 +24,7 @@ func (e *EmojiProcessor) Process(text string) string {
 	i := -1
 	return parser.ReplaceAllStringFunc(text, func(s string) string {
 		i++
-		key := "_$"+strconv.Itoa(i)+"_"
+		key := " _$"+strconv.Itoa(i)+"_"
 		e.container[key] = s
 		return key
 	})

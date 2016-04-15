aafdfbdda5fe01dd7631057e8d4e2e5824f5035c
Yury Kozyrev <yury.kozyrev@home24.de>
2016-04-15 08:58:54 +0200

    segments newlines

--- src/translator/processing/processing_segments.go
+++ src/translator/processing/processing_segments.go
@@ -2,12 +2,35 @@
 // Proprietary license.
 package processing
 
+import "strings"
+
 //import "unicode"
 
 type ProcessingSegments struct {
 
 }
 
-func (p *ProcessingSegments) some(){
+func (p *ProcessingSegments) Split(s string) []strings {
 	//unicode.IsGraphic([]rune("s"))
+	return strings.FieldsFunc(s, p.CombineFuncs(IsNewLine))
+}
+
+func (p *ProcessingSegments) CombineFuncs(fns... func(rune) bool) func(rune) bool {
+	return func(r rune) bool {
+		for fn := range fns {
+			if fn(r) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+func IsNewLine(r rune) bool {
+	switch r {
+	case '\n', 0x85:
+		return true
+	}
+	return false
+
 }
